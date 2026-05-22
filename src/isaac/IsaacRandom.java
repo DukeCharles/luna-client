a00a4c6f4aa2fe9626173bb6b15defe7a8c5f690
DukeCharles <charles.simon.morin@gmail.com>
2026-05-21 20:03:59 -0400

    Refactor IsaacRandom class: rename file for consistency; update package declaration

--- src/isaac/IsaacRandom.java
+++ src/isaac/IsaacRandom.java
@@ -1,4 +1,4 @@
-// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
+package isaac;// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
 // Jad home page: http://www.kpdus.com/jad.html
 // Decompiler options: packimports(3) 
 
